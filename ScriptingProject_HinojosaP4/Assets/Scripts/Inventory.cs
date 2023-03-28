6bbcbc79fac167cb6efb0ccda185f68e193feb86
BronzePotato <BronzeVulture101@gmail.com>
2023-03-28 12:19:31 -0700

    fgf

---
+++ ScriptingProject_HinojosaP4/Assets/Scripts/Inventory.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public class Stuff
+    {
+        public int bullets;
+        public int gernades;
+        public int rockets;
+        public float fuel;
+
+        public Stuff(int bul, int gre, int roc)
+        {
+            bullets = bul;
+            gernades = gre;
+            rockets = roc;
+        }
+        public Stuff()
+        {
+            bullets = 1;
+            gernades = 1;
+            rockets = 1;
+        }
+    }
+
+    public Stuff myStuff = new Stuff(50, 5, 5);
+
+    public Stuff myOtherStuff = new Stuff(50, 1.5f);
+    
+    void Start()
+    {
+        Debug.Log(myStuff.bullets);
+    }
+}
